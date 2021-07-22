cbd1dc1acdf39628db4920f53ff0043ae5262d0b
Stephen Chua <stephenchua@Stephens-MacBook-Pro.local>
2021-07-22 10:28:04 +0800

    combines

--- hackathon_split.py
+++ hackathon_split.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-threshold = 10
-
 df_income = pd.read_csv("./df_income_elim.csv")
 df_balance = pd.read_csv("./df_balance_elim.csv")
 df_cashflow = pd.read_csv("./df_cashflow_elim.csv")
@@ -10,20 +8,32 @@ print(df_income.shape)
 print(df_balance.shape)
 print(df_cashflow.shape)
 
-tickers_income = df_income['Ticker']
-for ticker in tickers_income:
-    df = df_income.loc[df_income['Ticker'] == ticker].sort_values(by='Report_Date')
-    df.tail(1).to_csv("./hackathon_test/df_income_" + ticker + ".csv", index=False)
-    df.iloc[:-1].to_csv("./hackathon_train/df_income_" + ticker + ".csv", index=False)
+df_income = df_income.groupby('Ticker').apply(lambda x: x[:-1])
+df_balance = df_balance.groupby('Ticker').apply(lambda x: x[:-1])
+df_cashflow = df_cashflow.groupby('Ticker').apply(lambda x: x[:-1])
 
-tickers_balance = df_balance['Ticker']
-for ticker in tickers_balance:
-    df = df_balance.loc[df_balance['Ticker'] == ticker].sort_values(by='Report_Date')
-    df.tail(1).to_csv("./hackathon_test/df_balance_" + ticker + ".csv", index=False)
-    df.iloc[:-1].to_csv("./hackathon_train/df_balance_" + ticker + ".csv", index=False)
+print(df_income.shape)
+print(df_balance.shape)
+print(df_cashflow.shape)
 
-tickers_cashflow = df_cashflow['Ticker']
-for ticker in tickers_cashflow:
-    df = df_cashflow.loc[df_cashflow['Ticker'] == ticker].sort_values(by='Report_Date')
-    df.tail(1).to_csv("./hackathon_test/df_cashflow_" + ticker + ".csv", index=False)
-    df.iloc[:-1].to_csv("./hackathon_train/df_cashflow_" + ticker + ".csv", index=False)
+df_income.to_csv("./combined_train_except_last/df_income_train_except_last.csv", index=False)
+df_balance.to_csv("./combined_train_except_last/df_balance_train_except_last.csv", index=False)
+df_cashflow.to_csv("./combined_train_except_last/df_cashflow_train_except_last.csv", index=False)
+
+# tickers_income = df_income['Ticker']
+# for ticker in tickers_income:
+#     df = df_income.loc[df_income['Ticker'] == ticker].sort_values(by='Report_Date')
+#     df.tail(1).to_csv("./hackathon_test/df_income_" + ticker + ".csv", index=False)
+#     df.iloc[:-1].to_csv("./hackathon_train/df_income_" + ticker + ".csv", index=False)
+#
+# tickers_balance = df_balance['Ticker']
+# for ticker in tickers_balance:
+#     df = df_balance.loc[df_balance['Ticker'] == ticker].sort_values(by='Report_Date')
+#     df.tail(1).to_csv("./hackathon_test/df_balance_" + ticker + ".csv", index=False)
+#     df.iloc[:-1].to_csv("./hackathon_train/df_balance_" + ticker + ".csv", index=False)
+#
+# tickers_cashflow = df_cashflow['Ticker']
+# for ticker in tickers_cashflow:
+#     df = df_cashflow.loc[df_cashflow['Ticker'] == ticker].sort_values(by='Report_Date')
+#     df.tail(1).to_csv("./hackathon_test/df_cashflow_" + ticker + ".csv", index=False)
+#     df.iloc[:-1].to_csv("./hackathon_train/df_cashflow_" + ticker + ".csv", index=False)
